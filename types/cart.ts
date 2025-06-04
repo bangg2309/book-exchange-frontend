d44d8da71cf90e5105e12d00817a9a8ab97535ef
Tran Qui Bang <21130279@st.hcmuaf.edu.vn>
2025-06-05 05:40:01 +0700

    feat(cart): Build UI cart and handle API cart service

---
+++ types/cart.ts
@@ -0,0 +1,16 @@
+export interface CartItemResponse {
+    id: number;
+    bookId: number;
+    bookTitle: string;
+    thumbnail: string;
+    description: string;
+    priceNew: number | null;
+    price: number;
+    quantity: number;
+    sellerName: string | null;
+  }
+
+  export interface CartAdditionRequest {
+    userId: number;
+    bookId: number;
+  }
